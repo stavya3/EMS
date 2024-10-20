288f5e0d9a02f3a9756825b576391a819474fdf1
stavya3 <stavyakc03@gmail.com>
2024-10-20 06:45:07 +0530

    Finished the basic design of the login page

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log("Email is",email);
+    console.log("Password is",password);
+    setEmail("");
+    setPassword("");
+  }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
         <div className='border-2 rounded-xl border-teal-600 p-20'>
-            <form className='flex flex-col items-center justify-center'>
-                <input required className='text-white outline-none bg-transparent border-2 border-teal-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
-                <input required className='text-white outline-none bg-transparent border-2 border-teal-600 text-xl py-3 px-5 mt-3 rounded-full placeholder:text-gray-400' type="password" placeholder='Enter Password' />
+            <form onSubmit={(e) => {submitHandler(e)}} className='flex flex-col items-center justify-center'>
+                <input value={email} onChange={(e) => {setEmail(e.target.value)}} required className='text-white outline-none bg-transparent border-2 border-teal-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
+                <input value={password} onChange={(e) => {setPassword(e.target.value)}} required className='text-white outline-none bg-transparent border-2 border-teal-600 text-xl py-3 px-5 mt-3 rounded-full placeholder:text-gray-400' type="password" placeholder='Enter Password' />
                 <button className='text-white outline-none bg-teal-600 text-xl py-3 px-10 rounded-full mt-7 '>Log in</button>
             </form>
         </div>
