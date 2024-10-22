3f19016dbb0444e963aa083e6bff3a4145ad9e2e
stavya3 <stavyakc03@gmail.com>
2024-10-22 07:18:41 +0530

    Added emp and admin datat for localStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 
+
 const App = () => {
   return (
     <>
---
+++ frontend/src/utils/localStorage.jsx
@@ -0,0 +1,35 @@
+const employee = [
+    {
+      "id": 1,
+      "email": "employee1@example.com",
+      "password": "123"
+    },
+    {
+      "id": 2,
+      "email": "employee2@example.com",
+      "password": "123"
+    },
+    {
+      "id": 3,
+      "email": "employee3@example.com",
+      "password": "123"
+    },
+    {
+      "id": 4,
+      "email": "employee4@example.com",
+      "password": "123"
+    },
+    {
+      "id": 5,
+      "email": "employee5@example.com",
+      "password": "123"
+    }
+]
+
+const admin = [
+    {
+      "id": 0,
+      "email": "admin@example.com",
+      "password": "123"
+    }
+]
