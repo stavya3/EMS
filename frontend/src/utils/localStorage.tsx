1ce7f7009241fb8408b7b710ff19d5bfa6cee797
stavya3 <stavyakc03@gmail.com>
2024-10-22 07:51:59 +0530

    Wrote the setters and getters for data in the local storage also (coalescing operator)

--- frontend/src/utils/localStorage.tsx
+++ frontend/src/utils/localStorage.tsx
@@ -293,3 +293,15 @@ const admin = [
       "password": "123"
     }
 ]
+
+export const setLocalStorage = () => {
+    localStorage.setItem('employees', JSON.stringify(employees));
+    localStorage.setItem('admin', JSON.stringify(admin));
+}
+
+export const getLocalStorage = () => {
+  const employees = JSON.parse(localStorage.getItem('employees') ?? '[]');
+  const admin = JSON.parse(localStorage.getItem('admin') ?? '[]');
+
+  console.log(employees, admin)
+}
