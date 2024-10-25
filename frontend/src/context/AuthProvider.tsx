f8018494bb8b95a51ec4761dac07493899a215d9
stavya3 <stavyakc03@gmail.com>
2024-10-25 16:47:54 +0530

    Trying to set up context to get data of admins and emps

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -6,7 +6,7 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
 
     const [userData, setUserData] = useState(null);
-    setUserData(getLocalStorage());
+    const { employee:any, admin:any } = getLocalStorage();
   return (
     <div>
         <AuthContext.Provider value={"Stavya"}>
