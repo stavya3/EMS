636f3013b7d7450e0975128fb5d9673e8be70ec0
stavya3 <stavyakc03@gmail.com>
2024-10-26 16:40:01 +0530

    Figuring out TS

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,15 +1,47 @@
-import React, { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { getLocalStorage } from '../utils/localStorage';
 
+interface UserDataType{
+    employees: {    
+      "id": number,
+      "email": string,
+      "password": string,
+      "tasks": [
+        {
+          "active": boolean,
+          "new": boolean,
+          "completed": boolean,
+          "failed": boolean,
+          "taskTitle": string,
+          "taskDescription": string,
+          "taskDate": string,
+          "category": string}
+        ]}
 
-export const AuthContext = createContext(); 
-const AuthProvider = ({ children }) => {
+    admin: {
+      "id": number,
+      "email": string,
+      "password": string
+    };
+  }
 
-    const [userData, setUserData] = useState(null);
-    const { employee:any, admin:any } = getLocalStorage();
+export const AuthContext = createContext<UserDataType | null>(null); 
+const AuthProvider = ({ children }: any) => {
+
+  
+
+
+    const [userData, setUserData] = useState<UserDataType | null>();
+    
+    
+    useEffect(() => {
+      const { employees, admin } = getLocalStorage();
+      setUserData({employees,admin});
+    }, []);
+
   return (
     <div>
-        <AuthContext.Provider value={"Stavya"}>
+        <AuthContext.Provider value={userData}>
             {children}
         </AuthContext.Provider>
     </div>
